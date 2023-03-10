d8a430da84836a65e0b10d6f3df42280a4af7b7f
Ice Wolf <zzf0972@163.com>
2023-03-10 20:44:22 +0800

    fix setToZeroIfMaxFEL

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -27,7 +27,8 @@ public class IntListExercises {
     public static void setToZeroIfMaxFEL(IntList L) {
         IntList p = L;
         while (p != null) {
-            if (firstDigitEqualsLastDigit(max(p))) {
+            int max = max(p);
+            if (firstDigitEqualsLastDigit(max)) {
                 p.first = 0;
             }
             p = p.rest;
@@ -52,7 +53,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
